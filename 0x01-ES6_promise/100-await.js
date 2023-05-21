94583ca868466c5b264064a533b69a3afe790c26
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 <walwaalemayehu@gmail.com>
2023-05-21 11:00:51 +0000

    all task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,7 @@
+export default async function asyncUploadUser() {
+  try {
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
+}
